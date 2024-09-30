706f18bdbb81a1d447ed2dbc25b13d09cd35072c
il deniel <150442406+danielechiarion@users.noreply.github.com>
2024-09-30 08:51:44 +0200

    Update Chiarion_4E_Es10A_numDecrescente.c

---
+++ 2_iterazione/Chiarion_4E_Es10A_numDecrescente.c
@@ -0,0 +1,26 @@
+/* Dato N un numero intero positivo, calcolare e visualizzare la somma dei
+primi N numeri interi.  */
+
+int main(int argc, char *argv[])
+	/* dichiarazione variabili */
+	int input;
+	int somma=0;
+	
+	/* inserimento dati input */
+	do{
+		printf("Inserisci un numero maggiore di 1: ");
+		scanf("%d", &input);
+	}while(input<=0);
+	
+	/* calcolo la somma dei primi N
+	numeri interi */
+	for(int i=1;i<=input;i++){
+		somma+=i;
+	}
+	
+	/* restituzione dati output */
+	printf("La somma dei primi %d numeri interi e' %d", input, somma);
+    
+    return 0;
+
+}
