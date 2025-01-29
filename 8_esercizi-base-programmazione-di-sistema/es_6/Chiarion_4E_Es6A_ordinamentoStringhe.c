0af7ec06b91ef3551ea78fa79c5102bfd861ec0a
danielechiarion <daniele.chiarion@iisviolamarchesini.edu.it>
2025-01-29 16:53:13 +0100

    completed ex 6 of folder 8 of basic system calls

--- 8_esercizi-base-programmazione-di-sistema/es_6/Chiarion_4E_Es6A_ordinamentoStringhe.c
+++ 8_esercizi-base-programmazione-di-sistema/es_6/Chiarion_4E_Es6A_ordinamentoStringhe.c
@@ -1,18 +1,29 @@
 /* Scrivere un programma in linguaggio C che legge da argv una serie di stringhe,
-le ordina usando un algoritmo di ordinamento a scelta e scrive il risultato
+le ordina in senso crescente o decrescente in funzione del flag usato in argv[2].
+Si utilizzi un algoritmo di ordinamento a scelta. Si scriva il risultato
 sia a video che su un file il cui nome risiede in argv[1].
 
 Esempio:
-
-$ ./a.out file.txt ciao a tutti
+$ ./a.out file.txt --asc ciao a tutti
 a
 ciao
 tutti
 
-$ cat file.txt
+$ cat file.txt 
 a
 ciao
-tutti */
+tutti
+
+Esempio:
+$ ./a.out file.txt --desc ciao a tutti
+tutti
+ciao
+a
+
+$ cat file.txt 
+tutti
+ciao
+a*/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -25,54 +36,64 @@ tutti */
 const char FLAGCRESCENTE[] = "--asc";
 const char FLAGDECRESCENTE[] = "--desc";
 
-/* funzione che scambia le stringhe */
-void scambio(char **a, char **b)
-{
+/* metodo che scambia due stringhe */
+void scambia(char **a, char **b){
     char *temp = *a;
-    *a = *b;
-    *b = temp;
+    *a=*b;
+    *b=temp;
 }
 
-/* funzione che ordina le stringhe utilizzando
-il selection sort*/
-void ordinaStringhe(char *argv[], int size)
-{
-    for (int i = 3; i < size - 1; i++)
-    {
-        for (int j = i + 1; j < size; j++)
-        {
-            if ((strcmp(FLAGCRESCENTE, argv[2]) == 0 && strcmp(argv[i], argv[j]) > 0) || (strcmp(FLAGDECRESCENTE, argv[2])==0 && (argv[i], argv[j]) < 0))
-            {
-                scambio(&argv[i], &argv[j]);
+/* metodo che ordina le stringhe sfruttando 
+il selection sort */
+void ordinaStringhe(char *array[], int size, char *flag){
+    for(int i=0;i<size-1;i++){
+        for(int j=i+1;j<size;j++){
+            if((strcmp(flag, FLAGCRESCENTE) && strcmp(array[j], array[i])<0) || (strcmp(flag, FLAGDECRESCENTE) && strcmp(array[j], array[i])>0)){
+                scambia(&array[i], &array[j]);
             }
         }
     }
 }
 
-int main(int argc, char *argv[])
-{
-    if (argc < 4)
-    {
-        printf("Numero di argomenti non valido");
+int main(int argc, char *argv[]){
+    /* prima di tutto controllo che il numero di argomenti
+    sia valido. Perchè il codice funzioni occorre:
+    - 1 parametro per il file da eseguire
+    - 1 parametro per il file di destinazione in cui salvare i risultato
+    - 1 parametro per il flag che indica l'ordinamento da considerare (crescente o decrescente)
+    - 1 o più stringhe da ordinare
+    
+    Quindi occorrono almeno 4 parametri */
+    if(argc < 4){
+        printf("Numero di argomenti insufficiente\n");
         exit(0);
     }
-    /* controllo se il flag è corretto */
-    if(strcmp(argv[2], FLAGCRESCENTE)!=0 && strcmp(argv[2], FLAGDECRESCENTE)){
-        printf("Flag per l'ordinamento non valido\n");
+    /* controllo inoltre che il flag inserito come
+    terzo argomento sia uguale a quelli stabiliti di default
+    per indicare un ordinamento crescente o decrescente */
+    if(strcmp(argv[2], FLAGCRESCENTE)!=0 && strcmp(argv[2], FLAGDECRESCENTE)!=0){
+        printf("Flag per l'ordinamento errato\n");
         exit(0);
     }
 
-    ordinaStringhe(argv, argc);
+    /* creo un array di stringhe e copio
+    le stringhe di interesse in argv */
+    char *array[argc-3];
+    for(int i=3;i<argc;i++){
+        array[i-3] = argv[i];
+    }
 
-    int fd = open(argv[1], O_CREAT | O_WRONLY, 0777);
-    for (int i = 3; i < argc; i++)
-    {
-        /* salvo su file e stampo a video*/
-        write(fd, argv[i], sizeof(argv[i]));
+    ordinaStringhe(array, argc-3, argv[2]); //ordino le stringhe
+
+    /* scrivo il risultato a video e sul file indicato */
+    int fd = open(argv[1], O_WRONLY | O_CREAT | O_TRUNC, 0777);
+    for(int i=0;i<argc-3;i++){
+        write(fd, array[i], strlen(array[i]));
         write(fd, "\n", strlen("\n"));
-        write(1, argv[i], sizeof(argv[i]));
+        write(1, array[i], strlen(array[i]));
         write(1, "\n", strlen("\n"));
     }
     close(fd);
+
     return 0;
 }
