83b8ef681d5231794a6233939e5e2e254f35dc9f
danielechiarion <daniele.chiarion@iisviolamarchesini.edu.it>
2025-02-12 09:12:01 +0100

    change ex 3 of exec with wait

--- 9_exec/es_3/Chiarion_4E_Es3A_rimozioneFile.c
+++ 9_exec/es_3/Chiarion_4E_Es3A_rimozioneFile.c
@@ -1,4 +1,4 @@
-/* Scrivere un programma in linguaggio C che usa il comando 
+/* Scrivere un programma in linguaggio C che usa il comando
 della shell rm per eliminare tutti files presenti
 nell'intervallo [argv[1], argv[argc-1]]
 
@@ -8,23 +8,34 @@ $ ./a.out /tmp/1.txt /tmp/3.txt /tmp/2.txt */
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/wait.h>
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
     /* controllo che ci sia un numero
     di argomenti minimo che possa eliminare il file */
-    if(argc<2){
+    if (argc < 2)
+    {
         printf("Numero di argomenti insufficiente\n");
         exit(0);
     }
 
     int pid1 = getpid();
-    for(int i=1;i<argc && getpid()==pid1;i++){
-        int p = fork();
-        if(p==0){
+    int gruppoPID[argc - 2];
+    for (int i = 1; i < argc && getpid() == pid1; i++)
+    {
+        gruppoPID[i - 1] = fork();
+        if (gruppoPID[i - 1] == 0)
+        {
             execl("/usr/bin/rm", "rm", argv[i], NULL);
             exit(0);
         }
     }
-    
+
+    /* il padre aspetta tutti i figli */
+    for(int i=0;i<argc-2;i++){
+        wait(NULL);
+    }
+
     return 0;
 }
