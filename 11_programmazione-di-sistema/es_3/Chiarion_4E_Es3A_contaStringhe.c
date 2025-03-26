de2a755a9e0bf9fab2c58885ebfd578fb1742e48
danielechiarion <daniele.chiarion@iisviolamarchesini.edu.it>
2025-03-26 12:19:09 +0100

    correct ex 3

--- 11_programmazione-di-sistema/es_3/Chiarion_4E_Es3A_contaStringhe.c
+++ 11_programmazione-di-sistema/es_3/Chiarion_4E_Es3A_contaStringhe.c
@@ -8,7 +8,7 @@ L’applicazione deve contare il numero di volte in cui un insieme di stringhe
 che l’utente inserisce da terminale (una alla volta) compaiono all’interno
 del file passato come parametro in argv[1].
 Se in una riga del file una parola compare più volte, al fine del conteggio,
-considerare solamente la prima occorrenza. 
+considerare solamente la prima occorrenza.
 L’applicazione termina quando l’utente inserisce la stringa “fine”.
 L’applicazione deve visualizzare il numero di volte in cui ogni stringa
 compare nel file e anche, al termine dell’esecuzione, il numero totale di
@@ -21,65 +21,62 @@ stringhe trovate nel file. */
 #include <sys/wait.h>
 #include <string.h>
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
     /* controllo se il numero di argomenti è valido */
-    if(argc != 2){
+    if (argc != 2)
+    {
         printf("Numero di argomenti non valido\n");
         exit(0);
     }
 
     /* dichiarazione variabili e vettori */
     int occorrenzaStringa, occorrenzeTotali = 0;
-    char stringaInput[50], inputOccorrenza[50];
+    char stringaInput[50], inputOccorrenza[1000];
     int pid;
 
-    /* creazione della pipe per 
+    /* creazione della pipe per
     la comunicazione tra processi figli */
-    int p1p2[2];
-    int p2p0[2];
-    pipe(p1p2);
-    pipe(p2p0);
+    int p1p0[2];
+    pipe(p1p0);
 
-    printf("Cerca Stringhe %s", argv[1]);
-    do{
-        /* inserisco la stringa */
-        printf("\n\nInserisci la stringa da ricercare: ");
+    printf("Cerca stringhe %s", argv[1]);
+    while (1)
+    {
+        printf("\n\nInserisci la parola da ricercare: ");
         scanf("%s", stringaInput);
+        /* interrompo se trovo la parola fine*/
+        if (strcmp(stringaInput, "fine") == 0)
+        {
+            close(p1p0[0]);
+            close(p1p0[1]);
+            printf("\nSono state trovate %d occorrenze\n", occorrenzeTotali);
+            break;
+        }
 
+        /* apro un processo figlio e
+        conto il numero di occorrenze */
         pid = fork();
-        if(pid==0){
-            /* redireziono lo standard input */
-            close(p1p2[1]);
-            close(0);
-            dup(p1p2[0]);
-            close(p1p2[0]);
-
-            /* redireziono lo standard output */
-            close(p2p0[0]);
+        if (pid == 0)
+        {
+            /* redireziono lo standard output
+            al fd della pipe */
+            close(p1p0[0]);
             close(1);
-            dup(p2p0[1]);
-            close(p2p0[1]);
+            dup(p1p0[1]);
+            close(p1p0[1]);
 
-            execl("/usr/bin/wc", "wc", "-l", NULL);
+            execl("/usr/bin/grep", "grep", "-c", stringaInput, argv[1], (char *)0);
             return -1;
         }
 
-        close(p2p0[1]);
-        close(p1p2[1]);
-        close(p1p2[0]);
-
-        /* leggo dalla pipe con p2 l'occorrenza della stringa 
-        e aggiorno le variabili */
-        read(p2p0[0], inputOccorrenza, strlen(inputOccorrenza));
-        close(p2p0[0]);
+        /* chiudo i file descriptor non utilizzati
+        e leggo il numero di occorrenze trovate */
+        read(p1p0[0], inputOccorrenza, sizeof(inputOccorrenza));
         occorrenzaStringa = atoi(inputOccorrenza);
         occorrenzeTotali += occorrenzaStringa;
-        printf("\nLa stringa %s e' presente nel file %d volte", stringaInput, occorrenzaStringa);
-
-    }while(strcmp(stringaInput, "fine"));
-
-    /* riporto il risultato finale */
-    printf("\nIl numero totale di stringhe trovate e': %d", occorrenzeTotali);
+        printf("\nLa stringa %s compare %d volte nel file", stringaInput, occorrenzaStringa);
+    }
 
     return 0;
 }
